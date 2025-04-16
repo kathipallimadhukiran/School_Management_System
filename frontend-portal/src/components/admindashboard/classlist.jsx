548924a9b35f5bdc91cb0630a61e182109aac808
kathipallimadhukiran <22a91a04l9@aec.edu.in>
2025-04-17 01:49:34 +0530

    Add missing ClassList.module.css

--- frontend-portal/src/components/admindashboard/classlist.jsx
+++ frontend-portal/src/components/admindashboard/classlist.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styles from "./ClassList.module.css"; // Import CSS module
+import styles from "./classList.module.css"; // Import CSS module
 
 const ClassList = () => {
     const [classes, setClasses] = useState([]);
